3e1c22457ee31ec25b9794281ed78ed51f53f44b
Андрей Барышников <andrey1andrey@narod.ru>
2024-04-05 23:03:20 +0300

    54

--- Homework.PriceCalculator.API/Startup.cs
+++ Homework.PriceCalculator.API/Startup.cs
@@ -3,7 +3,6 @@ using Homework.PriceCalculator.Domain;
 using Homework.PriceCalculator.Domain.Seporated;
 using Homework.PriceCalculator.Domain.Services;
 using Homework.PriceCalculator.Domain.Services.Interfaces;
-using Homework.PriceCalculator.Infrastructure.Dal.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Workshop.Api.ActionFilters;
@@ -28,37 +27,26 @@ public class Startup
     public void ConfigureServices(IServiceCollection services)
     {
         // services.AddMvc().AddJsonOptions(x => x.JsonSerializerOptions.PropertyNamingPolicy.ToString());
-        services.AddMvc().AddMvcOptions(x =>
+        services
+            .AddDomain(_configuration)
+            .AddInfrastructure()
+            .AddControllers()
+            .AddMvcOptions(x =>
             {
                 x.Filters.Add(new ExceptionFilterAttribute());
                 x.Filters.Add(new ResponseTypeAttribute((int)HttpStatusCode.InternalServerError));
                 x.Filters.Add(new ResponseTypeAttribute((int)HttpStatusCode.BadRequest));
                 x.Filters.Add(new ProducesResponseTypeAttribute((int)HttpStatusCode.OK));
-            });
-        services.Configure<PriceCalculatorOptions>(_configuration.GetSection("PriceCalculatorOptions"));
+            }).Services
+            .AddEndpointsApiExplorer()
+            ;
         services.AddScoped(x => x.GetRequiredService<IOptionsSnapshot<PriceCalculatorOptions>>().Value);
         services.AddControllers();
-        services.AddEndpointsApiExplorer();
         services.AddSwaggerGen(o =>
         {
             o.CustomSchemaIds(x => x.FullName);
         });
-        // services.AddScoped<IPriceCalculatorService, PriceCalculatorService>(
-        //     x => new PriceCalculatorService(
-        //         x.GetRequiredService<IOptions<PriceCalculatorOptions>>(),
-        //         // _configuration.GetValue<double>("PriceCalculatorOptions:VolumeToPriceRatio"),
-        //         // _configuration.GetValue<double>("PriceCalculatorOptions:WeightToPriceRatio"),
-        //         x.GetRequiredService<IStorageRepository>()
-        //         ));
-        services.AddScoped<IPriceCalculatorService, PriceCalculatorService>(x =>
-        {
-            var options = x.GetRequiredService<IOptionsSnapshot<PriceCalculatorOptions>>().Value;
-            return new PriceCalculatorService(options, x.GetRequiredService<IStorageRepository>());
-        });
-        services.AddSingleton<IStorageRepository, StorageRepository>();
-        services.AddSingleton<IGoodsRepository, GoodsRepository>();
         services.AddHostedService<GoodsSyncHostedService>();
-        services.AddScoped<IGoodsService, GoodsService>();
         services.AddHttpContextAccessor();
     }
 
---
+++ Homework.PriceCalculator.Infrastructure/ServiceCollectionExtension.cs
@@ -0,0 +1,37 @@
+﻿using Homework.PriceCalculator.Domain.Seporated;
+using Homework.PriceCalculator.Domain.Services.Interfaces;
+using Homework.PriceCalculator.Infrastructure.Dal.Repositories;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+
+namespace Homework.PriceCalculator.Domain.Services;
+
+public static class ServiceCollectionExtension
+{
+    public static IServiceCollection AddDomain(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<PriceCalculatorOptions>(configuration.GetSection("PriceCalculatorOptions"));
+        // services.AddScoped<IPriceCalculatorService, PriceCalculatorService>(
+        //     x => new PriceCalculatorService(
+        //         x.GetRequiredService<IOptions<PriceCalculatorOptions>>(),
+        //         // _configuration.GetValue<double>("PriceCalculatorOptions:VolumeToPriceRatio"),
+        //         // _configuration.GetValue<double>("PriceCalculatorOptions:WeightToPriceRatio"),
+        //         x.GetRequiredService<IStorageRepository>()
+        //         ));
+        services.AddScoped<IPriceCalculatorService, PriceCalculatorService>(x =>
+        {
+            var options = x.GetRequiredService<IOptionsSnapshot<PriceCalculatorOptions>>().Value;
+            return new PriceCalculatorService(options, x.GetRequiredService<IStorageRepository>());
+        });
+        return services;
+    }
+    
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+    {
+        services.AddSingleton<IStorageRepository, StorageRepository>();
+        services.AddSingleton<IGoodsRepository, GoodsRepository>();
+        services.AddScoped<IGoodsService, GoodsService>();
+        return services;
+    }
+}
