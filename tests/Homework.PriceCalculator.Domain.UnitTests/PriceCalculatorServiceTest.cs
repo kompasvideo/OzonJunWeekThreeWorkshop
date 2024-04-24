3b38e7d34e99e40ec3c689b5807ddacd8773d87e
Андрей Барышников <andrey1andrey@narod.ru>
2024-04-24 22:07:47 +0300

    2_30

--- tests/Homework.PriceCalculator.Domain.UnitTests/PriceCalculatorServiceTest.cs
+++ tests/Homework.PriceCalculator.Domain.UnitTests/PriceCalculatorServiceTest.cs
@@ -1,70 +1,112 @@
+using AutoFixture;
 using Homework.PriceCalculator.Domain.Entities;
 using Homework.PriceCalculator.Domain.Exceptions;
 using Homework.PriceCalculator.Domain.Models;
 using Homework.PriceCalculator.Domain.Seporated;
 using Homework.PriceCalculator.Domain.Services;
+using Moq;
 
 namespace Homework.PriceCalculator.Domain.UnitTests;
 
 public class PriceCalculatorServiceTest
 {
-    public class GoodsRepositoryStub : IGoodsRepository
-    {
-        public void AddOrUpdate(GoodViewModel viewModel)
-        {
-            throw new NotImplementedException();
-        }
-
-        public ICollection<GoodViewModel> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public GoodViewModel Get(int id)
-        {
-            throw new NotImplementedException();
-        }
-    }
-    public class StorageRepositoryStub : IStorageRepository
-    {
-        public void Save(StorageEntity entity)
-        {
-            throw new NotImplementedException();
-        }
-
-        public StorageEntity[] Query()
-        {
-            throw new NotImplementedException();
-        }
-    }
-    
     [Fact]
     public void PriceCalculatorService_WhenGoodsIsEmpty_ThrowException()
     {
         // Arrange
         var options = new PriceCalculatorOptions();
-        var repository = new GoodsRepositoryStub();
-        var storage = new StorageRepositoryStub();
+        var repositoryMock = new Mock<IGoodsRepository>();
+        var storageMock = new Mock<IStorageRepository>();
         var goods = Array.Empty<GoodModel>();
-        var cut = new PriceCalculatorService(options, repository, storage);
+        var cut = new PriceCalculatorService(
+            options, 
+            repositoryMock.Object,
+            storageMock.Object);
 
         // Act
         // Assert
         Assert.Throws<DomainException>(() => cut.CalculatePrice(goods));
     }
     
-    [Fact]
-    public void PriceCalculatorService_WhenGoodsIsNotEmpty_Success()
+    [Theory]
+    [InlineData(1_0000, 1)]
+    [InlineData(10_0000, 10)]
+    public void PriceCalculatorService_WhenCalculatePriceByWeight_Success(int weight, int expected)
     {
         // Arrange
-        var options = new PriceCalculatorOptions();
-        var repository = new GoodsRepositoryStub();
-        var storage = new StorageRepositoryStub();
-        var goods = Array.Empty<GoodModel>();
-        var cut = new PriceCalculatorService(options, repository, storage);
+        var options = new PriceCalculatorOptions() {VolumeToPriceRatio = 1, WeightToPriceRatio = 1} ;
+        
+        var repositoryMock = new Mock<IGoodsRepository>();
+        var storageMock = new Mock<IStorageRepository>();
+        
+        var goods = new[] {new GoodModel( 1,1,1,weight )};
+        var cut = new PriceCalculatorService(
+            options, 
+            repositoryMock.Object,
+            storageMock.Object);
 
         // Act
+        var result = cut.CalculatePrice(goods);
+
         // Assert
-        Assert.Throws<DomainException>(() => cut.CalculatePrice(goods));
+        Assert.Equal(expected, result);
+    }
+    
+    [Theory]
+    [MemberData(nameof(CalculatePriceByVolumeData))]
+    public void PriceCalculatorService_WhenCalculatePriceByVolume_Success(GoodModel model, int expected)
+    {
+        // Arrange
+        var options = new PriceCalculatorOptions() {VolumeToPriceRatio = 1, WeightToPriceRatio = 1} ;
+        
+        var repositoryMock = new Mock<IGoodsRepository>();
+        var storageMock = new Mock<IStorageRepository>();
+        
+        var cut = new PriceCalculatorService(
+            options, 
+            repositoryMock.Object,
+            storageMock.Object);
+
+        // Act
+        var result = cut.CalculatePrice(new [] {model});
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    public static IEnumerable<object[]> CalculatePriceByVolumeData
+    {
+        get
+        {
+            yield return new object[] {new GoodModel( 10,10,10,1 ), 1};
+            yield return new object[] {new GoodModel( 100,10,10,1 ), 10};
+        }
+    }
+     
+    [Fact]
+    public void PriceCalculatorService_WhenAny_Success()
+    {
+        // Arrange
+        var options = new PriceCalculatorOptions() {VolumeToPriceRatio = 1, WeightToPriceRatio = 1} ;
+        
+        var repositoryMock = new Mock<IGoodsRepository>();
+        var storageMock = new Mock<IStorageRepository>();
+        
+        // var model = new Fixture().Create<GoodModel>();
+        var model = new Fixture()
+            .Build<GoodModel>()
+            .With(x => x.Length, 1)
+            .Create();
+        
+        var cut = new PriceCalculatorService(
+            options, 
+            repositoryMock.Object,
+            storageMock.Object);
+
+        // Act
+        var result = cut.CalculatePrice(new [] {model});
+
+        // Assert
+        //Assert.Equal(expected, result);
     }
 }
